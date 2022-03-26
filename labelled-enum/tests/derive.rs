1c8836203a5d90c88b6b718b9ee83dc723013837
myuon <ioi.joi.koi.loi@gmail.com>
2022-03-26 18:43:20 +0900

    docs: README

--- labelled-enum/tests/derive.rs
+++ labelled-enum/tests/derive.rs
@@ -1,6 +1,18 @@
 use labelled_enum::*;
 use std::str::FromStr;
 
+#[derive(ToString, FromStr, PartialEq, Debug)]
+enum Test {
+    Foo,
+    Bar,
+}
+
+#[test]
+fn test_basic() {
+    assert_eq!(Test::Foo.to_string(), "Foo");
+    assert_eq!(Test::from_str("Foo").unwrap(), Test::Foo);
+}
+
 #[derive(ToString, FromStr, Debug, PartialEq)]
 #[label(rename_all = "snake_case")]
 enum TestSnakeCase {
